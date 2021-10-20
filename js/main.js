cc79bca2c2690333fc18f2b3b050e3638d6c54d9
Hassan-AlAmer <hassan.m.809@gmail.com>
2021-10-20 15:43:42 +0300

    finish Contact Us

--- js/main.js
+++ js/main.js
@@ -22,5 +22,29 @@
             
         }, false);
       });
+
+      var forms = document.getElementsByClassName('contact-validation');      
+      var LoginValidation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            if (form.checkValidity() === false) {            
+                event.stopPropagation();                
+            }else{
+                var alertsuccess=document.getElementById("alertsuccess");
+                alertsuccess.classList.remove("d-none");
+                setTimeout(() => {
+                    alertsuccess.classList.add("d-none");
+                    form.classList.remove('was-validated');
+                    form.reset();
+                }, 5000);
+            }
+            form.classList.add('was-validated');
+
+            
+        }, false);
+      });
+
+
     }, false);
   })();
